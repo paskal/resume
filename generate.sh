b6ae2a3fdf31b3bbe5801c779f4f1215de50b5cd
Dmitry Verkhoturov <paskal.07@gmail.com>
2016-11-27 05:03:59 -0500

    updated cv generation script with particular formats

--- generate.sh
+++ generate.sh
@@ -1,5 +1,8 @@
 rm ../public/cv/*
-hackmyresume BUILD resume.json TO ../public/cv/resume.all -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
+hackmyresume BUILD resume.json TO ../public/cv/resume.html -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
+hackmyresume BUILD resume.json TO ../public/cv/resume.pdf -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
+hackmyresume BUILD resume.json TO ../public/cv/resume.doc -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
+hackmyresume BUILD resume.json TO ../public/cv/resume.txt -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
 xvfb-run wkhtmltopdf ../public/cv/resume.pdf.html ../public/cv/resume.pdf
 rm ../public/cv/resume.pdf.html
 hackmyresume ANALYZE resume.json
