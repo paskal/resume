42f2fcd4ff2cfc42864b3d1cfe429ba278d3dbed
Dmitry Verkhoturov <paskal.07@gmail.com>
2016-12-18 19:42:49 +0300

    changed resume generator, filename

--- generate.sh
+++ generate.sh
@@ -1,9 +1,5 @@
+mkdir -p ../public/cv/
 rm ../public/cv/*
-hackmyresume BUILD resume.json TO ../public/cv/elegant.html -t jsonresume-theme-elegant
-hackmyresume BUILD resume.json TO ../public/cv/kendall.html -t jsonresume-theme-kendall
-#hackmyresume BUILD resume.json TO ../public/cv/resume.pdf -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
-#hackmyresume BUILD resume.json TO ../public/cv/resume.doc -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
-#hackmyresume BUILD resume.json TO ../public/cv/resume.txt -t /usr/local/lib/node_modules/fresh-theme-bootstrap/ --css cerulean
-#xvfb-run wkhtmltopdf ../public/cv/resume.pdf.html ../public/cv/resume.pdf
-#rm ../public/cv/resume.pdf.html
-hackmyresume ANALYZE resume.json
+resume export --theme elegant ../public/cv/verhoturov.html
+#resume export --theme kendall ../public/cv/kendall.html
+xvfb-run wkhtmltopdf ../public/cv/verhoturov.html ../public/cv/verhoturov.pdf
