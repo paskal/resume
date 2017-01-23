e0b6dc716aa63c155834c78267c979575219c520
Dmitry Verkhoturov <paskal.07@gmail.com>
2017-01-23 13:47:06 +0300

    resume generate fix

--- generate.sh
+++ generate.sh
@@ -1,5 +1,4 @@
 mkdir -p ../public/cv/
 rm ../public/cv/*
-resume export --theme elegant ../public/cv/verhoturov.html
-#resume export --theme kendall ../public/cv/kendall.html
+resume export --theme kendall ../public/cv/verhoturov.html
 xvfb-run wkhtmltopdf ../public/cv/verhoturov.html ../public/cv/verhoturov.pdf
