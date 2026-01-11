e55c643b459b2eb48590157df902eaa1930e602e
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-01-11 22:15:46 +0100

    Update PDF scale to 0.94 and switch to rsync
    - Change PDF scale from 0.90 to 0.94 (maximum that fits on 2 pages)
    - Replace scp with rsync for better delta transfers

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo "Generating PDF resume..."
 docker compose run --rm --remove-orphans build-pdf
 
 echo "Uploading to terrty..."
-scp cv/paskal-cv.html cv/paskal-cv.pdf terrty:~/blog/public/cv/
+rsync -avz cv/paskal-cv.html cv/paskal-cv.pdf terrty:~/blog/public/cv/
 
 echo "Done! https://terrty.net/cv/paskal-cv.html"
